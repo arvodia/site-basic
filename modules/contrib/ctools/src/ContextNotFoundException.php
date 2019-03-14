2b1057453a55344ec8bf35d3f00ef3636a85f259
root <root@arvodia.net>
2019-03-14 22:21:43 +0100

    update module

---
+++ modules/contrib/ctools/src/ContextNotFoundException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Drupal\ctools;
+
+class ContextNotFoundException extends \Exception {
+    
+}
---
+++ modules/contrib/xmlsitemap/src/XmlSitemapException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Drupal\xmlsitemap;
+
+/**
+ * Base XmlSitemapException class.
+ */
+class XmlSitemapException extends \Exception {
+    
+}
